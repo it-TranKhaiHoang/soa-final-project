87cf8748000e0c6bebc59f24696ad03de2300011
Tran Khai Hoang <hoangbo74@gmail.com>
2023-04-23 09:38:10 +0700

    add Announcement modal

--- server/src/resources/models/Announcement.js
+++ server/src/resources/models/Announcement.js
@@ -1,6 +1,23 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const Announcement = new Schema({})
+const Announcement = new Schema({
+    sendBy: { type: mongoose.Types.ObjectId, ref: 'SchoolStaff' },
+    sendTo: { type: mongoose.Types.ObjectId, ref: 'Student' },
+    title: {
+        type: String,
+        required: true,
+    },
+    attachment: {
+        type: String,
+    },
+    body: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+    },
+});
 
 module.exports = mongoose.model('Announcement', Announcement);

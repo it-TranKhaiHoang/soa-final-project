dcf5c7db6d2e6085791c3eff43bf6dc065977b7a
Tran Khai Hoang <hoangbo74@gmail.com>
2023-04-23 09:34:24 +0700

    add Attendance modal

--- server/src/resources/models/Attendance.js
+++ server/src/resources/models/Attendance.js
@@ -1,6 +1,24 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const Attendance = new Schema({})
+const Attendance = new Schema(
+    {
+        student: { type: mongoose.Types.ObjectId, ref: 'Student' },
+        currentDate: {
+            type: Date,
+            default: Date.now,
+        },
+        isPresent: {
+            type: Boolean,
+            default: false,
+        },
+        description: {
+            type: String,
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
 
 module.exports = mongoose.model('Attendance', Attendance);

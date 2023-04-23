1ae29c49a07a51d13d66b85747d4124d676c0ba2
Tran Khai Hoang <hoangbo74@gmail.com>
2023-04-23 09:27:14 +0700

    add Schedule modal

--- server/src/resources/models/Schedule.js
+++ server/src/resources/models/Schedule.js
@@ -1,6 +1,32 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const Schedule = new Schema({})
+const Schedule = new Schema(
+    {
+        class: { type: mongoose.Types.ObjectId, ref: 'Class' },
+        subject: { type: mongoose.Types.ObjectId, ref: 'Subject' },
+        dayOfWeek: {
+            type: String,
+            required: true,
+            enum: [''],
+        },
+        session: {
+            type: String,
+            required: true,
+            enum: ['morning', 'afternoon'],
+        },
+        startAt: {
+            type: Date,
+            required: true,
+        },
+        endAt: {
+            type: Date,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
 
 module.exports = mongoose.model('Schedule', Schedule);

002ac2d1f2e467f8677e5e81d3618287c3d75a2e
Tran Khai Hoang <hoangbo74@gmail.com>
2023-04-23 09:22:06 +0700

    add Subject modal

--- server/src/resources/models/Subject.js
+++ server/src/resources/models/Subject.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Subject = new Schema({
-})
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    teacher: { type: mongoose.Types.ObjectId, ref: 'SchoolStaff' },
+});
 
 module.exports = mongoose.model('Subject', Subject);

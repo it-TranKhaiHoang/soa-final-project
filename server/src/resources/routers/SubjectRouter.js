29ddabff058f61b6b698e7b7c98a3d4d0fcc6dbe
Tran Khai Hoang <hoangbo74@gmail.com>
2023-04-25 01:26:30 +0700

    Subject Controllers

--- server/src/resources/routers/SubjectRouter.js
+++ server/src/resources/routers/SubjectRouter.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const SubjectController = require('../controllers/SubjectController');
 const router = express.Router();
+
+router.post('/create', SubjectController.postCreate);
+router.get('/list', SubjectController.getListAll);
 
 module.exports = router;

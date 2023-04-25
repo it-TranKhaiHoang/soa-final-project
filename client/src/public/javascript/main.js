7443260d3de64c4a97495ee40e2f590b87850fea
Tran Khai Hoang <hoangbo74@gmail.com>
2023-04-25 19:27:31 +0700

    UI principal annoucement management

--- client/src/public/javascript/main.js
+++ client/src/public/javascript/main.js
@@ -30,7 +30,95 @@ $(document).ready(function () {
             },
         ],
     });
+    $('#listPrincipalAnnouncement').DataTable({
+        columnDefs: [
+            {
+                targets: 3,
+                render: DataTable.render.datetime('Do MMM YYYY'),
+            },
+        ],
+    });
+    let table = $('#listTeacherSent').DataTable({
+        dom: 'Bfrtip',
+        select: true,
+        buttons: [
+            {
+                text: 'Select all',
+                action: function () {
+                    table.rows().select();
+                },
+            },
+            {
+                text: 'Select none',
+                action: function () {
+                    table.rows().deselect();
+                },
+            },
+            {
+                text: 'Save data',
+                action: function () {
+                    let count = table.rows({ selected: true }).data().toArray();
+                    let data = table.$('input, select').serialize();
+                    console.log(count);
+                },
+            },
+        ],
+        columnDefs: [
+            {
+                orderable: false,
+                className: 'select-checkbox',
+                targets: 0,
+            },
+        ],
+        select: {
+            style: 'os',
+            selector: 'td:first-child',
+        },
+        order: [[1, 'asc']],
+    });
 });
+
+$(document).ready(function () {
+    let table = $('#listParentSent').DataTable({
+        dom: 'Bfrtip',
+        select: true,
+        buttons: [
+            {
+                text: 'Select all',
+                action: function () {
+                    table.rows().select();
+                },
+            },
+            {
+                text: 'Select none',
+                action: function () {
+                    table.rows().deselect();
+                },
+            },
+            {
+                text: 'Save data',
+                action: function () {
+                    let count = table.rows({ selected: true }).data().toArray();
+                    let data = table.$('input, select').serialize();
+                    console.log(count);
+                },
+            },
+        ],
+        columnDefs: [
+            {
+                orderable: false,
+                className: 'select-checkbox',
+                targets: 0,
+            },
+        ],
+        select: {
+            style: 'os',
+            selector: 'td:first-child',
+        },
+        order: [[1, 'asc']],
+    });
+});
+
 
 $(document).ready(function () {
     let table = $('#listStudentCheckAttend').DataTable({
